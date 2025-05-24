e9bb50b864fd5ee61e7c5d62ca5cd29325b007b9
Samirghimire-5 <samirghimire9841@gmail.com>
2025-05-24 13:37:18 +0545

    add: redux presist

--- client/src/lib/redux/store.ts
+++ client/src/lib/redux/store.ts
@@ -1,11 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import userReducer from "./user/userSlice";
+import localStorage from "redux-persist/lib/storage";
+
+const presistConfig = {
+  key: "root",
+  version: 1,
+  storage: localStorage,
+  whiteList: ["user"],
+}
+
+const rootReducer = combineReducers({
+  user: userReducer,
+})
+
+const persistedReducer = persistReducer(presistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
